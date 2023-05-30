04e43d89550e1f5b3b00183ef052092f31ebd2fb
Charles-Luchiari <charles99na@gmail.com>
2023-05-30 11:09:09 -0300

    Atualizando a classe ProdutoRepository

--- src/main/java/com/github/hortykut/hortykut/repository/ProdutoRepository.java
+++ src/main/java/com/github/hortykut/hortykut/repository/ProdutoRepository.java
@@ -1,9 +1,13 @@
 package com.github.hortykut.hortykut.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.hortykut.hortykut.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+	public List<Produto> findByProduto(String produto);
+	
 }
