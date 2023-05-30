b414c132dfd9fb2da28e2103f49756f03a992a08
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-05-30 11:05:06 -0300

    Update UserDetailsImpl.java

--- src/main/java/com/generation/hortykut/security/UserDetailsImpl.java
+++ src/main/java/com/generation/hortykut/security/UserDetailsImpl.java
@@ -19,8 +19,7 @@ public class UserDetailsImpl implements UserDetails {
 		this.password = user.getSenha();		
 	}
 	
-	public UserDetailsImpl() {
-		
+	public UserDetailsImpl() {		
 	}
 	
 	@Override
